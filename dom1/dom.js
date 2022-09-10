be734600ad44d3446c6606835a64223105854370
yatharth <lakhatey@gmail.com>
2022-09-10 18:01:17 +0530

    started dom 1

---
+++ dom1/dom.js
@@ -0,0 +1,29 @@
+//  Examin the Document Objects
+// console.dir(document);
+/*
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log((document.all[10].textContent = "hello"));
+
+*/
+/*
+// console.log(document.forms)
+// console.log(document.links);
+// console.log(document.images);
+*/
+
+// get ele by ID
+// console.log(document.getElementById("header-title"));
+let headerTitle = document.getElementById("header-title");
+console.log(headerTitle);
+// headerTitle.textContent = "hello";
+// headerTitle.innerText = "BYE BYE";
+
+// NOTE :---- Inner text pays attention to style
+
+headerTitle.innerHTML = "<h5>Hello</h5>";
