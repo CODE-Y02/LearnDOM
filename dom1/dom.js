b22abf05a6a7fe667c84918edac960d500530d41
yatharth <lakhatey@gmail.com>
2022-09-10 18:43:47 +0530

    made addItem bold and color green

--- dom1/dom.js
+++ dom1/dom.js
@@ -34,3 +34,18 @@ let header = document.getElementById("main-header");
 // document.title = "DOM 1";
 
 header.style.borderBottom = "solid 3px black";
+
+// Get ele by class name
+// let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "Hello 2";
+
+// items[1].style.background = "yellow";
+
+let titles = document.getElementsByClassName("title");
+// console.log(titles);
+let addItem = titles[0];
+// console.log(addItem);
+addItem.style.fontWeight = "bold";
+addItem.style.color ="green";
