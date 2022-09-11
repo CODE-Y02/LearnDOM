bc6dbf7b3380ab38ad3cc4ac42bc8970721e1f7d
yatharth <lakhatey@gmail.com>
2022-09-11 22:16:22 +0530

    ParentElement

--- dom3/dom.js
+++ dom3/dom.js
@@ -2,7 +2,15 @@
 let itemList = document.querySelector("#items");
 
 // parentNode
+/*
 console.log(itemList.parentNode);
 itemList.parentNode.style.backgroundColor = "lightgreen";
 
 console.log(itemList.parentNode.parentNode.parentNode);
+*/
+
+// ParentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = "lightgreen";
+
+console.log(itemList.parentElement.parentElement);
