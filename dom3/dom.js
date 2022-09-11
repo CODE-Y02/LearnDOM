98d35cc5ad39ea69709b232af89c05f6e20ebd91
yatharth <lakhatey@gmail.com>
2022-09-11 22:14:13 +0530

    parentNode

--- dom3/dom.js
+++ dom3/dom.js
@@ -1,1 +1,8 @@
 // Traversing Dom
+let itemList = document.querySelector("#items");
+
+// parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "lightgreen";
+
+console.log(itemList.parentNode.parentNode.parentNode);
