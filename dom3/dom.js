50b4b0e342a8e12e062a498f2325d3f556e8b89e
yatharth <lakhatey@gmail.com>
2022-09-11 22:42:26 +0530

    childNodes and Children

--- dom3/dom.js
+++ dom3/dom.js
@@ -10,7 +10,17 @@ console.log(itemList.parentNode.parentNode.parentNode);
 */
 
 // ParentElement
+/*
 console.log(itemList.parentElement);
 itemList.parentElement.style.backgroundColor = "lightgreen";
 
 console.log(itemList.parentElement.parentElement);
+*/
+
+// childnodes
+// console.log(itemList.childNodes)
+
+// Children
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = "yellow";
