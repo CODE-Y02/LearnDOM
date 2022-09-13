06152f142ff1e3f2e76b3a0503805b5aef0353ab
yatharth <lakhatey@gmail.com>
2022-09-13 11:58:25 +0530

    add, delete new item iin list

--- dom4/dom.js
+++ dom4/dom.js
@@ -4,17 +4,53 @@ let itemList = document.getElementById("items");
 // form submit events
 form.addEventListener("submit", addItem);
 
+//delete event
+itemList.addEventListener("click", removeItem);
+// console.log(itemList);
+
 // addItem
 function addItem(e) {
   e.preventDefault();
 
   // get input value
-  let newItem = document.getElementById("item");
+  let newItem = document.getElementById("item").value;
 
   // create new li element
   let li = document.createElement("li");
   // add class
   li.className = "list-group-item";
 
-  console.log(li);
+  // console.log(li);
+
+  // add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  //create dl button ele
+  let deleteBtn = document.createElement("button");
+
+  // add class to del btn
+  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+
+  // appednd text node
+  deleteBtn.appendChild(document.createTextNode("X"));
+
+  // apppednd btn to li
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
+
+  // clear
+  document.getElementById("item").value = "";
+}
+
+// remove Item
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    // console.log("hello");
+
+    if (confirm("Are yoy sure ?")) {
+      let li = e.target.parentElement;
+      li.remove()
+    }
+  }
 }
