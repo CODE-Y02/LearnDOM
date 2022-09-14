f115f6718110a3f92e6c56d4233ad68cb2133341
yatharth <lakhatey@gmail.com>
2022-09-14 12:29:14 +0530

    stored multiple user in local storage

--- UserLister/index.js
+++ UserLister/index.js
@@ -1,6 +1,5 @@
 let form = document.getElementsByClassName("userForm")[0];
-console.log(form);
-let store = localStorage;
+// console.log(form);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,6 +10,37 @@ form.addEventListener("submit", (e) => {
     name: name.value,
     email: email.value,
   };
+  if (localStorage.getItem(user.email)) {
+    // if email alreay exist
+    alert("Email Already exist");
+    return -1;
+  }
+  localStorage.setItem(user.email, JSON.stringify(user));
+  showUserOnScreen(user);
 
-  localStorage.setItem("user", JSON.stringify(user));
+  name.value = "";
+  email.value = "";
+});
+
+function showUserOnScreen(user) {
+  const parentNode = document.getElementById("userList");
+  const childHtml = `<li class="userInfo"> ${user.name} <span>${user.email}  </span><button class="del btn">Delete</button></li>`;
+
+  parentNode.innerHTML = parentNode.innerHTML + childHtml;
+}
+
+// delete funcion
+let ul = document.getElementById("userList");
+ul.addEventListener("click", (e) => {
+  e.preventDefault();
+  // console.log(e.target.classList.contains("del"));
+  if (e.target.classList.contains("del")) {
+    //perform delete
+    let li = e.target.parentElement;
+    // console.log(li.children[0].innerText);
+    let userEmail = li.children[0].innerText;
+    localStorage.removeItem(userEmail);
+
+    li.remove();
+  }
 });
